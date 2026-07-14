c369ed34ee6cac3bbf39750c09dfb6db0a47b501
Yuki Lai <124452442+Yukii0212@users.noreply.github.com>
2026-07-14 17:37:51 +0800

    Test commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+
+//Test comment
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);

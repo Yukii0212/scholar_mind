781c23d1ed3ac4be631abfad8b7c531e549c949e
Yuki Lai <124452442+Yukii0212@users.noreply.github.com>
2026-07-25 11:39:14 +0800

    Added the ability to swipe to skip the current flashcard (#70)

---
+++ lib/features/flashcards/widgets/study_swipe_card_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+import '../../../core/widgets/swipe_cards/swipe_card_item.dart';
+
+class StudySwipeCardItem extends SwipeCardItem {
+  const StudySwipeCardItem({
+    required super.title,
+    required super.icon,
+    required super.child,
+    required this.onSkipped,
+    super.expandable = false,
+  });
+
+  final VoidCallback onSkipped;
+}
